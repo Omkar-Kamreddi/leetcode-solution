166ce5242a13ef8715ace5e8a8d8a54bd15385d2
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 10:10:27 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.java
@@ -0,0 +1,19 @@
+// Last updated: 8/14/2026, 10:10:27 AM
+class Solution {
+    public int jump(int[] nums) {
+
+        int jumps = 0;
+        int maxReach = 0;
+        int end = 0;
+
+        for(int i=0; i<nums.length - 1; i++){
+            maxReach = Math.max(maxReach , i + nums[i]);
+
+            if(i == end){
+                jumps++;
+                end = maxReach;
+            }
+        }
+        return jumps;
+    }
+}
