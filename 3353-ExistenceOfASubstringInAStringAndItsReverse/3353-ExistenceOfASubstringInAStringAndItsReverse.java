6788b76072c0c8da0c9a629f170d3612a360f4d4
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 09:59:08 +0530

    Create 3353-ExistenceOfASubstringInAStringAndItsReverse

---
+++ 3353-ExistenceOfASubstringInAStringAndItsReverse/3353-ExistenceOfASubstringInAStringAndItsReverse.java
@@ -0,0 +1,17 @@
+// Last updated: 8/14/2026, 9:59:08 AM
+class Solution {
+    public boolean isSubstringPresent(String s) {
+        
+        String reverse = new StringBuilder(s).reverse().toString();
+
+        for(int i=0; i< s.length()-1; i++){
+            String sub = s.substring(i,i+2);
+
+            if(reverse.contains(sub)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
