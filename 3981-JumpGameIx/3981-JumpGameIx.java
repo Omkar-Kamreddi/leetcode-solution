a17b0368b081993433ecbb3007cb8d502836e85c
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 09:57:13 +0530

    Create 3981-JumpGameIx

---
+++ 3981-JumpGameIx/3981-JumpGameIx.java
@@ -0,0 +1,39 @@
+// Last updated: 8/14/2026, 9:57:13 AM
+class Solution {
+    public int[] maxValue(int[] nums) {
+        int n = nums.length;
+
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        int[] res = new int[n];
+
+      
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = Math.max(pre[i - 1], nums[i]);
+        }
+
+        suf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = Math.min(suf[i + 1], nums[i]);
+        }
+
+        res[n - 1] = pre[n - 1];
+
+        // build answer
+        for (int i = n - 2; i >= 0; i--) {
+
+            // merge segment
+            if (pre[i] > suf[i + 1]) {
+                res[i] = res[i + 1];
+            }
+
+            // new segment
+            else {
+                res[i] = pre[i];
+            }
+        }
+
+        return res;
+    }
+}
