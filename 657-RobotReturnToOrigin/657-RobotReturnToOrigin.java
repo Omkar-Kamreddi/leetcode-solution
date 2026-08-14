1824656e5a95f761995daa7b0a378973f7dacdda
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 10:06:15 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,15 @@
+// Last updated: 8/14/2026, 10:06:15 AM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (char move : moves.toCharArray()) {
+            if (move == 'U') y++;
+            else if (move == 'D') y--;
+            else if (move == 'L') x--;
+            else if (move == 'R') x++;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
