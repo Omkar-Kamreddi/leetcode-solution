cd2ae16bcf8991a9d013c550fc0870b83cb6a9fd
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 10:04:49 +0530

    Create 1297-MaximumNumberOfBalloons

---
+++ 1297-MaximumNumberOfBalloons/1297-MaximumNumberOfBalloons.java
@@ -0,0 +1,18 @@
+// Last updated: 8/14/2026, 10:04:49 AM
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[26];
+
+        for (char ch : text.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        return Math.min(
+            Math.min(freq['b' - 'a'], freq['a' - 'a']),
+            Math.min(
+                Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                freq['n' - 'a']
+            )
+        );
+    }
+}
