be4ace1036662a182d38960628e2fd4b8fecc9b5
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 10:01:07 +0530

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.js
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2026, 10:01:06 AM
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length == 0){
+        return -1;
+    }
+
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
