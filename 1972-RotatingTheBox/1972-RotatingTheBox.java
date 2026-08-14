36f0eadf9abd885cf8941f246720cbd5d18a6574
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-08-14 10:03:05 +0530

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.java
@@ -0,0 +1,42 @@
+// Last updated: 8/14/2026, 10:03:05 AM
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+
+        int m = box.length;
+        int n = box[0].length;
+
+        // Step 1: Apply gravity (move stones to right)
+        for (int i = 0; i < m; i++) {
+
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (box[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                else if (box[i][j] == '#') {
+
+                    char temp = box[i][empty];
+                    box[i][empty] = '#';
+                    box[i][j] = temp;
+
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: Rotate 90 degree clockwise
+        char[][] ans = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                ans[j][m - 1 - i] = box[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
