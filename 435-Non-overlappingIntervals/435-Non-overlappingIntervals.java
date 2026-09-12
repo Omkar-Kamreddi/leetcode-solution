9ce361930797f8bc4495e795cc36b82041a0a7cb
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-09-12 20:53:05 +0530

    File updated at 9/12/2026, 8:53:05 PM

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,22 +1,36 @@
-// Last updated: 8/20/2026, 7:07:20 PM
+// Last updated: 9/12/2026, 8:53:05 PM
 1class Solution {
-2    public int eraseOverlapIntervals(int[][] intervals) {
-3        
-4        //Step 1) Sort by ending time
-5        Arrays.sort(intervals,(a,b)->a[1] - b[1]);
-6
-7        int ans = 0;
-8        int lastEnd = intervals[0][1];
+2
+3    //Two Pointer approach : Move the pointer based on whose interval ends first
+4    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+5
+6        List<int[]> result = new ArrayList<>();
+7
+8        int i =0, j = 0;
 9
-10        for(int i= 1; i<intervals.length; i++){
-11            if(intervals[i][0]>=lastEnd){
-12                //No Overlapping
-13                lastEnd = intervals[i][1];
-14            }else{
-15                //Overlapping
-16                ans++;
-17            }
-18        }
-19        return ans;
-20    }
-21}
+10        while(i < firstList.length && j < secondList.length){
+11            
+12            //Current interval
+13            int []first = firstList[i];
+14            int second[] = secondList[j];
+15
+16            //Find intersection
+17            int start = Math.max(first[0],second[0]);
+18            int end = Math.min(first[1],second[1]);
+19
+20            //valid interset
+21            if(start <= end){
+22                result.add(new int[]{start,end});
+23            }
+24
+25            //move interval that ends first
+26            if(first[1]<second[1]){
+27                i++;
+28            }else{
+29                j++;
+30            }
+31        }
+32        
+33        return result.toArray(new int[result.size()][]);
+34    }
+35}
