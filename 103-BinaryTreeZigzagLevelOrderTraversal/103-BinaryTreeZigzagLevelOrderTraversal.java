39d252e7f50086c5d5dfea61d9b3303ff00612e3
Omkar-Kamreddi <omkarkamreddi.nbnscoe.comp@gmail.com>
2026-09-14 17:55:11 +0530

    File updated at 9/14/2026, 5:55:10 PM

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,63 +1,70 @@
-// Last updated: 9/14/2026, 5:30:02 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-18        List<List<Integer>> result = new ArrayList<>();
-19
-20        //Base Case 
-21        if (root == null) {
-22            return result;
-23        }
-24
-25        Queue<TreeNode> queue= new LinkedList<>();
-26        queue.offer(root);
-27        int level = 0;
-28
-29        while(!queue.isEmpty()){
-30            
-31            int levelSize = queue.size();
+// Last updated: 9/14/2026, 5:55:10 PM
+1class Solution {
+2
+3    public int orangesRotting(int[][] grid) {
+4
+5        int minutes = 0;
+6        int fresh = 0;
+7        Queue<int[]> queue = new LinkedList<>();
+8
+9        for(int i=0; i<grid.length; i++){
+10            for(int j=0; j<grid[0].length; j++){
+11                if(grid[i][j] == 2){
+12                    queue.offer(new int[]{i,j});
+13                }
+14
+15                if(grid[i][j] == 1){
+16                    fresh++;
+17                }
+18            }
+19        }
+20
+21        //Define the directions
+22        int [][]directions = {{-1,0},{1,0},{0,-1},{0,1}};
+23
+24        while(!queue.isEmpty() && fresh > 0){
+25
+26            int levelSize = queue.size();
+27
+28            // One BFS level = one minute
+29            for (int i = 0; i < levelSize; i++) {
+30
+31            int [] curr = queue.poll();
 32
-33            List<Integer> currLevel = new ArrayList<>();
-34
-35            for(int i=0; i<levelSize; i++){
-36                TreeNode currNode = queue.poll();
-37
-38                currLevel.add(currNode.val);
-39    
-40                    //Always add left-right
-41                    if(currNode.left != null){
-42                        queue.offer(currNode.left);
-43                    }
-44
-45                    if(currNode.right != null){
-46                        queue.offer(currNode.right);
-47                    }
-48                
-49            }
-50
-51            // Reverse alternate levels
-52            if (level % 2 != 0) {
-53                Collections.reverse(currLevel);
-54            }
-55            
-56            level++;
-57            result.add(currLevel);
-58        }
-59
-60        return result;
-61    }
-62}
+33
+34            int r = curr[0];
+35            int c = curr[1];
+36
+37            for(int dir[] : directions){
+38
+39                int nr = r + dir[0];
+40                int nc = c + dir[1];
+41
+42                //out of bound
+43                if(nr < 0 || nc<0 || nr>= grid.length || nc>= grid[0].length){
+44                    continue;
+45                }
+46
+47                // Fresh orange
+48                    if (grid[nr][nc] == 1) {
+49
+50                        // Make it rotten
+51                        grid[nr][nc] = 2;
+52
+53                        fresh--;
+54
+55                        // It will spread in the NEXT minute
+56                        queue.offer(new int[]{nr, nc});
+57                    }
+58            }
+59            }
+60            minutes++;
+61        }
+62
+63        if(fresh > 0){
+64            return -1;
+65        }
+66        
+67        return minutes;
+68    }
+69}
